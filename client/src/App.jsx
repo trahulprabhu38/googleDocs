b53148d23710e2ea4c331831278e0380ba0e6c3c
T Rahul Prabhu <trahulprabhu38@gmail.com>
2025-01-14 03:40:02 +0530

    done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import TextEditor from './components/textEditor'
+import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
+import { v4 as uuidV4 } from "uuid";
+import TextEditor from "./components/textEditor";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-    <div className="header">Instant Sync</div>
-     <TextEditor/>
-    </>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<Navigate to={`/documents/${uuidV4()}`} />} />
+        <Route path="/documents/:id" element={<TextEditor />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
--- server/index.js
+++ server/index.js
@@ -1,16 +1,17 @@
+require('dotenv').config(); // Load environment variables
 
-
-const io = require('socket.io')(5050, {
+const io = require('socket.io')(process.env.PORT || 5050, {
     cors: {
-        origin:"http://localhost:5173",
-        methods:['GET','POST']
+        origin: process.env.CORS_ORIGIN || "https://baddocs.netlify.app/",
+        methods: ['GET', 'POST']
     },
-})
+});
 
-io.on("connection",(socket)=>{
-    socket.on('send-changes',delta=>{
-        console.log(delta)
-        socket.broadcast.emit('receive-changes',delta)
-    })
-    console.log("connected!")
-})
+io.on("connection", (socket) => {
+    socket.on('send-changes', (delta) => {
+        console.log(delta);
+        socket.broadcast.emit('receive-changes', delta);
+    });
+
+    console.log("connected!");
+});
